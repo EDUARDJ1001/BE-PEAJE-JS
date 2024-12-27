1ead6c8e5d9b60a13755f811ce6181cceda32816
EDUARDJ1001 <123607036+EDUARDJ1001@users.noreply.github.com>
2024-12-26 20:35:31 -0600

    Updated variables

--- config/db.js
+++ config/db.js
@@ -9,12 +9,12 @@ const connectDB = async () => {
     if (!pool) {
         try {
             pool = mysql.createPool({
-                host: process.env.DB_HOST,
-                user: process.env.DB_USER,
-                password: process.env.DB_PASSWORD,
-                database: process.env.DB_NAME,
-                port: process.env.DB_PORT,
-                waitForConnections: true, 
+                host: process.env.MYSQL_ADDON_HOST,
+                user: process.env.MYSQL_ADDON_USER,
+                password: process.env.MYSQL_ADDON_PASSWORD,
+                database: process.env.MYSQL_ADDON_DB,
+                port: process.env.MYSQL_ADDON_PORT,
+                waitForConnections: true,
                 connectionLimit: 10,
                 queueLimit: 0
             });

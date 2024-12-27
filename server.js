1fc18b6e48932dd2dd27ebd546da5a8fc08f4593
EDUARDJ1001 <123607036+EDUARDJ1001@users.noreply.github.com>
2024-12-26 20:53:02 -0600

    Update Server.js

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.get('/', (req, res) => {
 
 registerRoutes(app);
 
-const PORT = process.env.PORT || 4000;
-const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.MYSQL_ADDON_PORT || 4000;
+const HOST = process.env.MYSQL_ADDON_HOST || '0.0.0.0';
 
 app.listen(PORT, HOST, async () => {
     try {

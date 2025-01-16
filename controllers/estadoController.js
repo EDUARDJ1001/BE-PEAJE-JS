17bb633084b08b15398bbcaa3b0a8de50fb2618e
EDUARDJ1001 <123607036+EDUARDJ1001@users.noreply.github.com>
2025-01-16 12:40:11 -0600

    RollBack: estados

--- controllers/estadoController.js
+++ controllers/estadoController.js
@@ -8,31 +8,3 @@ export const obtenerEstado = async (req, res) => {
         res.status(500).json({ message: 'Error al obtener estados de vias', error});
     }
 }
-
-export const seleccionarVia = async (req, res) => {
-    const { via } = req.body;
-
-    try {
-        const connection = await connectDB();
-        const query = 'SELECT * FROM Estado_Via WHERE Id = ?';
-        const [rows] = await connection.query(query, [via]);
-
-        if (rows.length === 0) {
-            return res.status(404).json({ message: 'Vía no encontrada.' });
-        }
-
-        const estadoVia = rows[0].Descripcion;
-        if (estadoVia === "Activa" || estadoVia === "Revision") {
-            return res.status(403).json({ message: 'La vía está en estado "Activa" o "Revisión", no puedes seleccionarla.' });
-        }
-
-        const updateQuery = 'UPDATE Estado_Via SET Descripcion = "Activa" WHERE Id = ?';
-        await connection.query(updateQuery, [via]);
-
-        return res.status(200).json({ message: 'Vía seleccionada correctamente.' });
-    } catch (error) {
-        console.error('Error al seleccionar la vía:', error);
-        return res.status(500).json({ message: 'Error al seleccionar la vía', error });
-    }
-};
-
--- routes/estadoRoutes.js
+++ routes/estadoRoutes.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import { obtenerEstado } from '../controllers/estadoController.js';
-import { seleccionarVia } from '../controllers/estadoController.js';
 
 const router = express.Router();
 
 router.get('/', obtenerEstado);
-router.post('/seleccionar-via', seleccionarVia);
 
 export default router;

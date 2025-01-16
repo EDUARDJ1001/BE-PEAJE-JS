3180dce9f3b29a27d40e33f0defe7a1c1d6de40e
EDUARDJ1001 <123607036+EDUARDJ1001@users.noreply.github.com>
2025-01-16 10:40:41 -0600

    ADD: Validacion de vias

--- controllers/estadoController.js
+++ controllers/estadoController.js
@@ -1,10 +1,29 @@
 import { obtenerEstado as obtenerEstadoModel } from "../models/estadoModel.js";
+import connectDB from "../config/db.js";
 
-export const obtenerEstado = async (req, res) => {
+export const seleccionarVia = async (req, res) => {
+    const { via } = req.body;
+
     try {
-        const estados = await obtenerEstadoModel();
-        res.status(200).json(estados);
+        const connection = await connectDB();
+        const query = 'SELECT * FROM Estado_Via WHERE Id = ?';
+        const [rows] = await connection.query(query, [via]);
+
+        if (rows.length === 0) {
+            return res.status(404).json({ message: 'Vía no encontrada.' });
+        }
+
+        const estadoVia = rows[0].Descripcion;
+        if (estadoVia === "Activa" || estadoVia === "Revision") {
+            return res.status(403).json({ message: 'La vía está en estado "Activa" o "Revisión", no puedes seleccionarla.' });
+        }
+
+        const updateQuery = 'UPDATE Estado_Via SET Descripcion = "Activa" WHERE Id = ?';
+        await connection.query(updateQuery, [via]);
+
+        return res.status(200).json({ message: 'Vía seleccionada correctamente.' });
     } catch (error) {
-        res.status(500).json({ message: 'Error al obtener estados de vias', error});
+        console.error('Error al seleccionar la vía:', error);
+        return res.status(500).json({ message: 'Error al seleccionar la vía', error });
     }
-}
+};
--- routes/estadoRoutes.js
+++ routes/estadoRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import { obtenerEstado } from '../controllers/estadoController.js';
 
+import { seleccionarVia } from '../controllers/estadoController.js';
+
 const router = express.Router();
 
 router.get('/', obtenerEstado);
+router.post('/seleccionar-via', seleccionarVia);
 
 export default router;

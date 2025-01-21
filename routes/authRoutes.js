de70f26aa98ad52578792c30ffad4c8e9b7aecca
EDUARDJ1001 <123607036+EDUARDJ1001@users.noreply.github.com>
2025-01-21 09:42:19 -0600

    Remove Store Token

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { login, storeToken, updateSelectedVia } from '../controllers/authController.js';
+import { login, updateSelectedVia } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/login', login);
-router.post('/store-token', storeToken);
 router.post('/updateSelectedVia', updateSelectedVia);
 
 export default router;

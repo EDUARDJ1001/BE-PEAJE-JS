f68df2ee7958f6ef3376db466307ad22e7b653cb
EDUARDJ1001 <123607036+EDUARDJ1001@users.noreply.github.com>
2025-01-31 14:31:17 -0600

    Habilitar CORS

--- server.js
+++ server.js
@@ -8,16 +8,13 @@ dotenv.config();
 
 const app = express();
 
-// app.use(
-//     cors({
-//         origin: 'https://sistema-peaje.vercel.app',
-//         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//         allowedHeaders: ['Content-Type', 'Authorization'],
-//     })
-// );
-
-app.use(cors({}));
-
+app.use(
+    cors({
+        origin: 'https://sistema-peaje.vercel.app',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+);
 
 app.use(express.json());
 

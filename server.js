78151a300051036ac575b2d188434b58d2467620
EDUARDJ1001 <123607036+EDUARDJ1001@users.noreply.github.com>
2025-02-23 23:07:14 -0600

    DESAHBILITAR CORS PARA PRUEBAS

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-    cors({
-        origin: 'https://sistema-peaje.vercel.app',
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'https://sistema-peaje.vercel.app',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//         allowedHeaders: ['Content-Type', 'Authorization'],
+//     })
+// );
+
+app.use(cors({}));
 
 app.use(express.json());
 
